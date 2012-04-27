96c8c82f00d3ebc196081c1f4ecf79b747e0f2c9
noxx <noxx@noxx-desktop.(none)>
2012-04-27 14:28:42 +0200

    Verbesserungsvorschlag von Felix umgesetzt

--- m-files/transformation/transformation.m
+++ m-files/transformation/transformation.m
@@ -3,9 +3,9 @@ function [drehmatrix,offset]=transformation(fokus)
     % neue z-Achse zeigt zum Ursprung
     z=-fokus/norm(fokus);
     % neue y-Achse ist die Normale der ebene aus neuer und alter z-Achse
-    y=-cross([0;0;1],z)/norm(cross([0;0;1],z));
+    x=cross([0;0;1],z)/norm(cross([0;0;1],z));
     % neue x-Achse ist die Normale der Ebene aus neuer y- und neuer z-Achse
-    x=-cross(z,y)/norm(cross(z,y));
+    y=cross(z,x);
     
     drehmatrix=[x y z];
 
