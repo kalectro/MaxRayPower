e51ba40ba0cc12c9be01a148da4b2b86a01d59a9
John Preston <mor1pheus@googlemail.com>
2012-05-26 04:32:19 +0200

    Korrekte Drehrichtung fuer zweiten Spiegel rausgefunden. Hab mir ueberlegt, dass das Origin (0 0 0 ) im neuen koord-system die koordiaten (0 0 norm(focus))* haben muesste und hab daran alles angepasst :) einigermassene korrektheit kann man am hochgeladenen Bild ueberpruefen. Gute Nacht, F

--- m-files/smallmirrorreflection/smallmirrorreflection.m
+++ m-files/smallmirrorreflection/smallmirrorreflection.m
@@ -3,21 +3,25 @@
 %% new_focus nicht initialisiert!!!
 
 
-function [rays_from_mirror]=smallmirrorreflection(rays_to_mirror,focus,small_mirr_hand)
+function [rays_from_mirror,ind_of_rays_from_small_mirror]=smallmirrorreflection(rays_to_mirror,focus,small_mirr_hand,ind_of_rays_that_hit_it)
     % Transformationsmatrix für den kleinen Spiegel erzeugen
     [rot_matrix]=transformation(focus);
-    
+
     % Strahlen in die Koordinaten des kleinen Spiegels transformieren
     for i=1:length(rays_to_mirror(1,1,:))
-        rays_to_mirror(:,1,i)=rot_matrix*rays_to_mirror(:,1,i)+ focus; % Stützvektor
-        rays_to_mirror(:,2,i)=rot_matrix*rays_to_mirror(:,2,i); % Richtungsvektor
+        %Matlab sagt ich soll mit A\b anstatt mit inv(A)*b invertieren...
+        rays_to_mirror(:,1,i)=rot_matrix\(rays_to_mirror(:,1,i)- focus); % Stützvektor
+        rays_to_mirror(:,2,i)=rot_matrix\rays_to_mirror(:,2,i); % Richtungsvektor
     end
     
     % Strahlen mit dem kleinen Spiegel kollidieren
-    half_small_mirr_edge_length = sqrt(1)/2; %10qm Grundflaeche
+    half_small_mirr_edge_length = sqrt(1)/2; %1qm Grundflaeche
     small_mirr_borders = [-half_small_mirr_edge_length half_small_mirr_edge_length -half_small_mirr_edge_length half_small_mirr_edge_length];
     [collision_points,ind_rays_that_hit_it]=collision_tracker_dychotom(rays_to_mirror,small_mirr_hand,small_mirr_borders);
-    
+        %um die indces im Bezug auf die Sonne mitzuschleifen -> für
+        %Backtracing!
+        ind_of_rays_from_small_mirror = ind_of_rays_that_hit_it(ind_rays_that_hit_it);
+        
     % Strahlen am kleinen Spiegel reflektieren
     tmp=zeros(3,2,length(ind_rays_that_hit_it)); % Matrix mit Strahlen, die reflektiert werden können
     for i=1:length(ind_rays_that_hit_it)
@@ -30,9 +34,9 @@ function [rays_from_mirror]=smallmirrorreflection(rays_to_mirror,focus,small_mir
     rays_from_mirror=reflection_m(tmp,collision_points,small_mirr_hand);
     
     % Strahlen in die ursprünglichen Koordinaten umrechnen
-    tmp=inv(rot_matrix);
+%     tmp=inv(rot_matrix);
     for i=1:length(rays_from_mirror(1,1,:))
-        rays_from_mirror(:,1,i)=tmp*(rays_from_mirror(:,1,i)- focus);
-        rays_from_mirror(:,2,i)=tmp*rays_from_mirror(:,2,i);
+        rays_from_mirror(:,1,i)=rot_matrix*rays_from_mirror(:,1,i)+focus;
+        rays_from_mirror(:,2,i)=rot_matrix*rays_from_mirror(:,2,i);
     end
 end
