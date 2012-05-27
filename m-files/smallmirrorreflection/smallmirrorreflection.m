8d0fed0151abe74aacb3d731c57c408e8e8e4364
John Preston <mor1pheus@googlemail.com>
2012-05-27 13:31:58 +0200

    Hilfsplot im small mirror Koordinatensystem auskommentiert

--- m-files/smallmirrorreflection/smallmirrorreflection.m
+++ m-files/smallmirrorreflection/smallmirrorreflection.m
@@ -21,26 +21,26 @@ function [rays_from_mirror,ind_of_rays_from_small_mirror]=smallmirrorreflection(
 
         ind_of_rays_from_small_mirror = ind_of_rays_that_hit_it(ind_rays_that_hit_it);
     
-        %zwischendurch mal im small_mirror_KS den Stand plotten
-        small_mirror_surface = zeros(10);
-        [rays_x rays_y] = meshgrid(linspace(-half_small_mirr_edge_length, half_small_mirr_edge_length, 10));
-        for x_ind = 1:10
-            for y_ind = 1:10
-                %Oberfläche berechnen
-                small_mirror_surface(x_ind,y_ind) = small_mirr_hand(rays_x(x_ind,y_ind),rays_y(x_ind,y_ind));
-            end
-        end
-        figure(2)
-        hold on
-        surf(rays_x,rays_y,small_mirror_surface,'FaceColor','red','EdgeColor','none','FaceAlpha',0.5);
-        axis equal
-        axis([-1 1 -1 1 -4 4])
-        arrow3(permute(rays_to_mirror(:,1,:),[1 3 2])',permute(rays_to_mirror(:,1,:),[1 3 2])'+permute(rays_to_mirror(:,2,:),[1 3 2])','g',0.5,0.5)
-        arrow3(permute(rays_to_mirror(:,1,ind_rays_that_hit_it),[1 3 2])',collision_points','y4',2,2)
-        view(3)
-        camlight
-        hold off
-        figure(1)
+%         %zwischendurch mal im small_mirror_KS den Stand plotten
+%         small_mirror_surface = zeros(10);
+%         [rays_x rays_y] = meshgrid(linspace(-half_small_mirr_edge_length, half_small_mirr_edge_length, 10));
+%         for x_ind = 1:10
+%             for y_ind = 1:10
+%                 %Oberfläche berechnen
+%                 small_mirror_surface(x_ind,y_ind) = small_mirr_hand(rays_x(x_ind,y_ind),rays_y(x_ind,y_ind));
+%             end
+%         end
+%         figure(2)
+%         hold on
+%         surf(rays_x,rays_y,small_mirror_surface,'FaceColor','red','EdgeColor','none','FaceAlpha',0.5);
+%         axis equal
+%         axis([-1 1 -1 1 -4 4])
+%         arrow3(permute(rays_to_mirror(:,1,:),[1 3 2])',permute(rays_to_mirror(:,1,:),[1 3 2])'+permute(rays_to_mirror(:,2,:),[1 3 2])','g',0.5,0.5)
+%         arrow3(permute(rays_to_mirror(:,1,ind_rays_that_hit_it),[1 3 2])',collision_points','y4',2,2)
+%         view(3)
+%         camlight
+%         hold off
+%         figure(1)
 
     % Strahlen am kleinen Spiegel reflektieren
     tmp=zeros(3,2,length(ind_rays_that_hit_it)); % Matrix mit Strahlen, die reflektiert werden können
@@ -51,7 +51,7 @@ function [rays_from_mirror,ind_of_rays_from_small_mirror]=smallmirrorreflection(
     
     % etwas andere reflection-Funktion. Sonst verschwendet man vorhandenes Wissen%
 %     rays_from_mirror=reflection_m(tmp,collision_points,small_mirr_hand);
-test=permute(tmp(:,2,:),[1 3 2]);
+
     ray_dirs_from_mirror = reflection([tmp(:,2,:) permute(collision_points,[1 3 2])],small_mirr_hand);
     
     test=permute(ray_dirs_from_mirror,[1 3 2]);
