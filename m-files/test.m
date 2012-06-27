1a2af4d17f5c80efee149e6a8aca68b8fd48ef92
Maxim Dolgov <ugccv@rz-y-305.stud.uni-karlsruhe.de>
2012-06-27 22:05:16 +0200

    neuer Test

--- m-files/test.m
+++ m-files/test.m
@@ -1,11 +1,14 @@
 close all;
 modus.ordnung = 2;
 modus.absorber_optimieren = false;
-modus.kleinen_spiegel_optimieren = true;
 modus.grossen_spiegel_optimieren = true;
+modus.kleinen_spiegel_optimieren = true;
 modus.spiegelpfad_radius_optimieren = true;
 modus.num_rays_per_row = 20;
 modus.number_zeitpunkte = 15;
 
-A = [0 1/20 0 0 0 1/20 0 0 0 1/20 0 0 0 1/20 0 0 2];
+A = [0 0 0 1/20 1/20 0 0 0 ...
+     0 0 0 1/20 1/20 0 0 0 ...
+     2];
+ 
 Objective_function(A, modus);
