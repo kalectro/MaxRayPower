403ae9dbd42159f83f08ee2b71cad465ec23134a
Maxim <the_noxx@mail.ru>
2012-06-27 23:51:08 +0200

    Dateiausgabe von Debugging-Infos

--- m-files/test_random_start.m
+++ m-files/test_random_start.m
@@ -24,5 +24,21 @@ if modus.kleinen_spiegel_optimieren
 end
 
 A = [randi(1000,1,randomNum)/1000 , (randi(45,1,1)/10 + .5)];
+
+fid = fopen('optimization_start.txt','a+');
+fprintf(fid, '##########################\n');
+fprintf(fid, 'Parameter der Optimierung:\n');
+fprintf(fid, '--------------------------\n');
+fprintf(fid, 'modus.ordnung: %d\n', modus.ordnung);
+fprintf(fid, 'modus.absorber_optimieren: %d\n', modus.absorber_optimieren);
+fprintf(fid, 'modus.grossen_spiegel_optimieren: %d\n', modus.grossen_spiegel_optimieren);
+fprintf(fid, 'modus.kleinen_spiegel_optimieren: %d\n', modus.kleinen_spiegel_optimieren);
+fprintf(fid, 'modus.spiegelpfad_radius_optimieren: %d\n', modus.spiegelpfad_radius_optimieren);
+fprintf(fid, 'modus.num_rays_per_row: %d\n', modus.num_rays_per_row);
+fprintf(fid, 'modus.number_zeitpunkte: %d\n', modus.number_zeitpunkte);
+fprintf(fid, 'A = [ ');
+fprintf(fid,'%f ',A);
+fprintf(fid,']\n##########################\n\n');
+fclose(fid);
  
-Objective_function(A, modus);
+% Objective_function(A, modus);
