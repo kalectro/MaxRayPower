6730f6063275cadf29324e103bed573779ddbb58
John Preston <mor1pheus@googlemail.com>
2012-06-27 21:23:05 +0200

    Testfunktion fuer ObjectiveFunction jetzt mit Modus-struct

--- m-files/test.m
+++ m-files/test.m
@@ -5,7 +5,7 @@ modus.kleinen_spiegel_optimieren = true;
 modus.grossen_spiegel_optimieren = true;
 modus.spiegelpfad_radius_optimieren = true;
 modus.num_rays_per_row = 20;
-modus.number_zeitpunkte = 5;
+modus.number_zeitpunkte = 15;
 
-A = [0 1/20 0 0 0 1/20 0 0 0 1/20 0 0 0 1/20 0 0 1];
+A = [0 1/20 0 0 0 1/20 0 0 0 1/20 0 0 0 1/20 0 0 2];
 Objective_function(A, modus);
